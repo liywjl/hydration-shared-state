4070f88a25c9d6346b69a05a8a1f8c36746400f7
William Lacroix <william.j.t.lacroix@gmail.com>
2020-12-09 17:45:52 +0000

    update app1

--- src/App1.tsx
+++ src/App1.tsx
@@ -1,12 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { emitter } from "./sharedData";
 
 export const App1 = () => {
+  const [count, setCount] = useState<undefined | number>();
+
   console.log("App1 is rendered");
   useEffect(() => {
     const listener = emitter.on("foo", console.log);
     return () => emitter.off(listener);
-  });
-  return <h1>App1</h1>;
+  }, [setCount]);
+
+  return (
+    <>
+      <h1>App2</h1>
+      {count && <p>{count}</p>}
+    </>
+  );
 };
